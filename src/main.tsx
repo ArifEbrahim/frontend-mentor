bd277c7fa59f870aa12b74ca20d7eefee1fe4076
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-11-13 21:21:02 +0000

    refactor: restructure routing and remove unused App component; add ProductPreview page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import ProductPreview from './pages/product-preview'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>welcome</div>
+  },
+  {
+    path: '/product-preview',
+    element: <ProductPreview />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />,
+  </StrictMode>
 )
---
+++ src/pages/product-preview/index.tsx
@@ -0,0 +1,3 @@
+export default function ProductPreview () {
+  return <h1>hello world</h1>
+}
