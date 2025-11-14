18c643a7442a4c18aa905e8f89426ae205245b5a
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-11-14 22:00:43 +0000

    refactor: improve QR code component structure and styling WIP

--- src/pages/qr-code/index.tsx
+++ src/pages/qr-code/index.tsx
@@ -3,14 +3,12 @@ import qrCode from './image-qr-code.png'
 export default function QRCode() {
   return (
     <div className="bg-[#D5E1EF] min-h-full min-w-full fixed flex flex-col items-center justify-center">
-      <div className='bg-white'>
-        <div>
-          <div>
-            <img src={qrCode} alt="QR code" />
-          </div>
+      <div className="bg-white flex flex-col items-center mx-7 h-[500px] text-center rounded-2xl p-4">
+        <div className="overflow-hidden">
+          <img src={qrCode} alt="QR code" />
         </div>
-        <div>Improve your front-end skills by building projects</div>
-        <div>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+        <p>Improve your front-end skills by building projects</p>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
     </div>
   )
