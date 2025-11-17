41910b5121caf627a2296a2331b6d01eb76ff1b7
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-11-17 07:17:22 +0000

    style: enhance QR code page layout and typography, mobile view complete

--- src/pages/qr-code/index.tsx
+++ src/pages/qr-code/index.tsx
@@ -2,13 +2,17 @@ import qrCode from './image-qr-code.png'
 
 export default function QRCode() {
   return (
-    <div className="bg-[#D5E1EF] min-h-full min-w-full fixed flex flex-col items-center justify-center">
-      <div className="bg-white flex flex-col items-center mx-7 h-[500px] text-center rounded-2xl p-4">
-        <div className="overflow-hidden">
+    <div className="bg-[hsl(212,45%,89%)] min-h-full min-w-full fixed flex flex-col items-center justify-center">
+      <div className="bg-white flex flex-col items-center mx-7 text-center rounded-2xl p-4 shadow-lg">
+        <div className="overflow-hidden h-[302] w-[302] rounded-2xl mb-6">
           <img src={qrCode} alt="QR code" />
         </div>
-        <p>Improve your front-end skills by building projects</p>
-        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <p className="font-display font-bold text-xl text-[hsl(218,44%,22%)] text-center mb-4">
+          Improve your front-end skills by building projects
+        </p>
+        <p className="font-display font-normal text-sm text-[hsl(216,15%,48%)] text-center mb-4">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
       </div>
     </div>
   )
