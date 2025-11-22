3483521b49af9722e92512caba249002ad016a96
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-11-22 05:39:38 +0000

    style: update QR code component structure and improve accessibility

--- src/pages/qr-code/index.tsx
+++ src/pages/qr-code/index.tsx
@@ -2,20 +2,20 @@ import qrCode from './image-qr-code.png'
 
 export default function QRCode() {
   return (
-    <div className="fixed flex min-h-full min-w-full flex-col items-center justify-center bg-[hsl(212,45%,89%)]">
-      <div className="flex max-w-[300px] flex-col items-center rounded-2xl bg-white p-4 text-center shadow-lg">
-        <div className="mb-2 h-[302] w-[302] overflow-hidden rounded-2xl">
-          <img src={qrCode} alt="QR code" />
+    <main className="min-h-screen flex flex-col items-center justify-center bg-[hsl(212,45%,89%)]">
+      <article className="flex max-w-[300px] flex-col items-center rounded-2xl bg-white p-4 text-center shadow-lg">
+        <div role="img" className="mb-2 overflow-hidden rounded-2xl">
+          <img src={qrCode} alt="QR code linking to Frontend Mentor" />
         </div>
         <div className="p-4">
-          <p className="mb-4 text-center font-display text-lg leading-tight font-bold text-[hsl(218,44%,22%)]">
+          <h1 className="mb-4 text-center font-display text-lg leading-tight font-bold text-[hsl(218,44%,22%)]">
             Improve your front-end skills by building projects
-          </p>
+          </h1>
           <p className="text-center font-display text-sm leading-tight font-normal text-[hsl(216,15%,48%)]">
             Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
           </p>
         </div>
-      </div>
-    </div>
+      </article>
+    </main>
   )
 }
