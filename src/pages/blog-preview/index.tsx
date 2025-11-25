fd6d06e436e3fe5e980803bbfa70d292eb1d09ad
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-11-25 04:47:10 +0000

    style: reorder tailwindcss import and enhance BlogPreview component layout and content

--- src/pages/blog-preview/index.tsx
+++ src/pages/blog-preview/index.tsx
@@ -1,21 +1,25 @@
 import illustration from './illustration-article.svg'
 import avatar from './image-avatar.webp'
 
-export default function BlogPreview(){
+export default function BlogPreview() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-[hsl(47,88%,63%)]">
-      <article className="flex max-w-[300px] flex-col items-center rounded-2xl bg-white p-4 text-center shadow-lg border border-black">
-        <div role="img" className="mb-2 overflow-hidden rounded-2xl">
-          <img src={illustration} alt='An illustration'/>
-          <p>Learning</p>
-          <p>Published 21 Dec 2023</p>
-          <h1>HTML & CSS foundations</h1>
-          <p>These languages are the backbone of every website, defining structure, content and presentation</p>
-          <div>
-            <img src={avatar} alt='Avatar of the blog creator'/>
-            <p>Greg Hooper</p>
-          </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[hsl(47,88%,63%)]">
+      <article className="flex max-w-[340px] flex-col items-center rounded-2xl border bg-white p-5 text-left font-figtree shadow-lg">
+        <div role="img" className="mb-5 overflow-hidden rounded-2xl">
+          <img src={illustration} alt="An illustration" />
         </div>
+        <section className="text-left">
+          <p className="mb-3 inline-block rounded-md bg-[hsl(47,88%,63%)] px-3 py-1 text-sm font-bold">Learning</p>
+          <p className="mb-3 text-xs">Published 21 Dec 2023</p>
+          <h1 className="mb-3 text-xl font-extrabold">HTML & CSS foundations</h1>
+          <p className="mb-3 text-sm leading-6 text-[hsl(0,0%,42%)]">
+            These languages are the backbone of every website, defining structure, content and presentation
+          </p>
+          <div className="flex items-center">
+            <img src={avatar} alt="Avatar of the blog creator" className="mr-3 h-9" />
+            <p className="text-sm font-extrabold">Greg Hooper</p>
+          </div>
+        </section>
       </article>
     </main>
   )
