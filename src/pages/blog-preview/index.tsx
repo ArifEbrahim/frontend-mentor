1a3d02b2991dc93f7d342a15207799433f91612b
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-11-29 18:17:34 +0000

    style: update styles and challenge complete

--- src/pages/blog-preview/index.tsx
+++ src/pages/blog-preview/index.tsx
@@ -4,26 +4,21 @@ import avatar from './image-avatar.webp'
 export default function BlogPreview() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[hsl(47,88%,63%)]">
-      <article className="flex max-w-[340px] flex-col items-center rounded-2xl border bg-white p-5 text-left font-figtree shadow-lg">
-        <div role="img" className="mb-5 overflow-hidden rounded-2xl">
-          <img src={illustration} alt="An illustration" />
+      <article className="max-w-[350px] rounded-2xl border bg-white p-6 font-figtree shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]">
+        <img src={illustration} className="mb-4 h-52 rounded-2xl object-cover" alt="Abstract shapes" />
+        <span className="mb-4 inline-block rounded-md bg-[hsl(47,88%,63%)] px-3 py-1 text-sm font-bold">Learning</span>
+        <time dateTime="2023-12-21" className="mb-4 block text-xs">
+          Published 21 Dec 2023
+        </time>
+        <h1 className="mb-4 text-xl font-extrabold hover:text-[hsl(47,88%,63%)]">HTML & CSS foundations</h1>
+        <p className="mb-4 text-sm leading-5 text-[hsl(0,0%,42%)]">
+          These languages are the backbone of every website, defining structure, content and presentation
+        </p>
+        <div className="flex items-center gap-3">
+          <img src={avatar} alt="Avatar of the blog creator" className="h-8 w-8" />
+          <p className="text-sm font-extrabold">Greg Hooper</p>
         </div>
-        <section className="text-left">
-          <p className="mb-3 inline-block rounded-md bg-[hsl(47,88%,63%)] px-3 py-1 text-sm font-bold">Learning</p>
-          <p className="mb-3 text-xs">Published 21 Dec 2023</p>
-          <h1 className="mb-3 text-xl font-extrabold hover:text-[hsl(47,88%,63%)]">HTML & CSS foundations</h1>
-          <p className="mb-3 text-sm leading-6 text-[hsl(0,0%,42%)]">
-            These languages are the backbone of every website, defining structure, content and presentation
-          </p>
-          <div className="flex items-center">
-            <img src={avatar} alt="Avatar of the blog creator" className="mr-3 h-9" />
-            <p className="text-sm font-extrabold">Greg Hooper</p>
-          </div>
-        </section>
       </article>
     </main>
   )
 }
-
-// TODO: add proper box shadow
-// TODO: increase image size
