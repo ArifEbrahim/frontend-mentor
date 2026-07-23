5514be95c002ba7ea153102dbcaac97c9de650f7
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2026-07-23 05:35:17 +0100

    fix: copy update on not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -4,8 +4,10 @@ export default function NotFound() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 py-12">
       <h1 className="mb-8 text-center text-3xl font-bold text-gray-800">Page not found</h1>
-      <p className='mb-8'>Sorry...something went wrong</p>
-      <Link className='border p-4 rounded-xl bg-gray-800 text-white font-bold' to='/'>Back to home page</Link>
+      <p className="mb-8">Sorry, we couldn’t find that page.</p>
+      <Link className="rounded-xl border bg-gray-800 p-4 font-bold text-white" to="/">
+        Back to home page
+      </Link>
     </main>
   )
 }
