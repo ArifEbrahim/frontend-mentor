ab95a95c0d9c8239d4bfde3906b51a2747108fb8
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2026-07-24 05:29:21 +0100

    fix: updated react router to fix broken navigation

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,22 @@ import NotFound from './pages/not-found'
 import { routes } from './routes'
 import './main.css'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />
+    },
+    ...routes,
+    {
+      path: '*',
+      element: <NotFound />
+    }
+  ],
   {
-    path: '/',
-    element: <Home />
-  },
-  ...routes,
-  {
-    path: '*',
-    element: <NotFound />
+    basename: '/frontend-mentor/'
   }
-])
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
